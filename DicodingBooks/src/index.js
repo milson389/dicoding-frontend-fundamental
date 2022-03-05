4a29697ed5e9a29799d28b0034ed88e17909867a
Audie Milson <mugetsu3008@gmail.com>
2022-03-05 15:25:13 +0700

    Fetch API

---
+++ DicodingBooks/src/index.js
@@ -0,0 +1,11 @@
+/*
+        jangan ubah kode di bawah ini ya!
+*/
+
+import "regenerator-runtime";
+import "./scripts/bootstrap.bundle.min.js";
+import "./styles/bootstrap.min.css";
+import "./styles/main.css";
+import main from "./scripts/main";
+
+main();
